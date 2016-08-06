1f5d8d85d852e09bb680ddd577cd6280f53c79d1
mtuan <tranminhtuan684@gmail.com>
2016-08-06 10:49:24 +0700

    Add Tags model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806023329) do
+ActiveRecord::Schema.define(version: 20160806034845) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "content"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
